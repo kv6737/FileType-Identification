0c5bc47a430f731f86ae92a6ea5d4f73bdb8ef64
mukul1232 <36454983+mukul1232@users.noreply.github.com>
2020-09-02 01:29:36 +0530

    Update FileIdentification.java

--- java/src/com/project/FileIdentification.java
+++ java/src/com/project/FileIdentification.java
@@ -8,13 +8,18 @@ import java.util.*;
 public class FileIdentification {
 
 	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = br.readLine();//the input file name should be of the form file_name.file_extension like abc.pdf,abc.txt etc
-		String file[] = input.split(".");
-		String fileName = file[0];
-		String extension = file[1];
-		Data data = new Data(fileName, extension);
-		System.out.println("Developer " + data.getDeveloper());
-	}
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String input = br.readLine();//the input file name should be of the form file_name.file_extension like abc.pdf,abc.txt etc
+			String file[] = input.split(".");
+			String fileName = file[0];
+			String extension = file[1];
+			
+			Data data = new Data(fileName, extension);
+			System.out.println("Developer " + data.getDeveloper());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+}
 
 }
