1682af8a5d307816e5cc1dec457e57944daa695b
muskan-71 <muskanarora7199@gmail.com>
2020-09-02 13:28:30 +0530

    sources update

---
+++ java/src/com/project/sources.java
@@ -0,0 +1,24 @@
+package com.project;
+
+import com.project.Data;
+
+class source1 extends Data{
+	source1()
+	{
+		
+	}
+}
+class source2 extends Data
+{
+	source2()
+	{
+		
+	}
+}
+class source3
+{
+	source3()
+	{
+		
+	}
+}
