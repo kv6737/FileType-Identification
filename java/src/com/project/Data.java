ccc5b3b97bddf7f7dc5d89572e018bc144b6d5c3
muskan-71 <muskanarora7199@gmail.com>
2020-09-02 13:45:01 +0530

    update

--- java/src/com/project/Data.java
+++ java/src/com/project/Data.java
@@ -19,22 +19,13 @@ class Data
 	private List<String> associatedApplications;
 	Data(String fileName,String extension)
 	{
+		this.description=null;
+		this.category=null;
+		this.language=null;
+		this.paradigm=null;
+		this.developer=null;
 		this.associatedApplications = new ArrayList<String>();
-		try
-		{
-			String html = "https://fileinfo.com/extension/"+extension;
-			Document doc = Jsoup.connect(html).userAgent("Mozilla/5.0").get();
-			Elements e1 = doc.select("table.headerInfo").get(0).getElementsByTag("tr");
-			this.developer = e1.get(0).getElementsByTag("td").get(1).text();
-			Elements e2=doc.select("div.infoBox").get(0).getElementsByTag("p");
-			this.description=e2.get(0).text();
-			//this.category=
-			
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
+		
 	}
 	public String getCategory()
 	{
