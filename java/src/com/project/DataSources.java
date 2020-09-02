79dd8d5aa8ef769e3b4fc38c4e7e82129204d13d
muskan-71 <muskanarora7199@gmail.com>
2020-09-02 14:09:01 +0530

    update

--- java/src/com/project/DataSources.java
+++ java/src/com/project/DataSources.java
@@ -31,3 +31,48 @@ class Source1 extends Data
 		}
 	}
 }
+class Source2 extends Data
+{
+	Source2(String fileName,String extension)
+	{
+		this.associatedApplications = new ArrayList<String>();
+		try
+		{
+			String html = "https://fileinfo.com/extension/"+extension;
+			Document doc = Jsoup.connect(html).userAgent("Mozilla/5.0").get();
+			Elements e1 = doc.select("table.headerInfo").get(0).getElementsByTag("tr");
+			this.developer = e1.get(0).getElementsByTag("td").get(1).text();
+			Elements e2=doc.select("div.infoBox").get(0).getElementsByTag("p");
+			this.description=e2.get(0).text();
+			//this.category=
+			
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+class Source3 extends Data
+{
+	Source3(String fileName,String extension)
+	{
+		this.associatedApplications = new ArrayList<String>();
+		try
+		{
+			String html = "https://fileinfo.com/extension/"+extension;
+			Document doc = Jsoup.connect(html).userAgent("Mozilla/5.0").get();
+			Elements e1 = doc.select("table.headerInfo").get(0).getElementsByTag("tr");
+			this.developer = e1.get(0).getElementsByTag("td").get(1).text();
+			Elements e2=doc.select("div.infoBox").get(0).getElementsByTag("p");
+			this.description=e2.get(0).text();
+			//this.category=
+			
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
