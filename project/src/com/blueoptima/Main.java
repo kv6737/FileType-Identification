428768faed6bee74d95143598c239610d835cda0
mukul1232 <36454983+mukul1232@users.noreply.github.com>
2020-09-03 13:01:52 +0530

    delete

--- project/src/com/blueoptima/Main.java
+++ project/src/com/blueoptima/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
-import com.blueoptima.Data;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
